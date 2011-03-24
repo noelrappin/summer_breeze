c8ec26a4563e208fe19705a6a678ba1e6dc658af
Noel Rappin <nrappin@obtiva.com>
2011-03-24 15:02:27 -0500

    now with less stupid

--- lib/summer_breeze.rb
+++ lib/summer_breeze.rb
@@ -1,12 +1,7 @@
-require 'summer_breeze'
-require 'rails'
-p "loading 2"
 module SummerBreeze
   module Rails
     class Railtie < ::Rails::Railtie
-      p "in class"
       rake_tasks do
-        p "in block"
         load "summer_breeze/tasks/summer_breeze.rake"
       end
     end
--- lib/summer_breeze/tasks/summer_breeze.rake
+++ lib/summer_breeze/tasks/summer_breeze.rake
@@ -1,13 +1,7 @@
-p "file"
-
-task :aaa do 
-  p "I'm a test"
-end
-
 namespace :summer_breeze do
   
   task :generate_fixtures => :environment do
-    p "fred"
+    p "testing"
   end
 
 end
