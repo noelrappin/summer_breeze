3707e0547ee2283826259db77097954a4145f374
Noel Rappin <nrappin@obtiva.com>
2011-03-24 14:52:15 -0500

    still trying to figure this out

--- lib/summer_breeze.rb
+++ lib/summer_breeze.rb
@@ -1,10 +1,12 @@
 require 'summer_breeze'
 require 'rails'
-p "loading"
+p "loading 2"
 module SummerBreeze
   module Rails
     class Railtie < ::Rails::Railtie
+      p "in class"
       rake_tasks do
+        p "in block"
         load "summer_breeze/tasks/summer_breeze.rake"
       end
     end
--- lib/summer_breeze/tasks/summer_breeze.rake
+++ lib/summer_breeze/tasks/summer_breeze.rake
@@ -1,9 +1,13 @@
 p "file"
 
+task :aaa do 
+  p "I'm a test"
+end
+
 namespace :summer_breeze do
   
   task :generate_fixtures => :environment do
-    
+    p "fred"
   end
 
 end
